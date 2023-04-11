efcb1ac14ae3c730164f3610e20e38f73e862d63
Elouan Pauna <pnl4817a@ens.fsi>
2023-04-11 19:00:45 +0200

    les humains ont des connaissances

--- src/histoire/HistoireTP4.java
+++ src/histoire/HistoireTP4.java
@@ -8,13 +8,18 @@ import personnages.Yakuza;
 public class HistoireTP4 {
 
 	public static void main(String[] args) {
-		Humain prof = new Humain("Prof","kombucha",54);
-		prof.DireBonjour();
-		prof.acheter("une boisson", 12);
-		prof.Boire();
-		prof.acheter("un jeu",2);
-		prof.acheter("un kimono", 50);
-		
+		Commercant marco = new Commercant("Marco", 20);
+		Commercant chonin = new Commercant("Chonin", 40);
+		Commercant kumi = new Commercant("Kumi", 10);
+		Yakuza yaku = new Yakuza("Yaku Le Noir", "whisky", 30, "Warsong");
+		Ronin roro = new Ronin("Roro", "shochu", 60);
+		marco.faireConnaissanceAvec(roro);
+		marco.faireConnaissanceAvec(yaku);
+		marco.faireConnaissanceAvec(chonin);
+		marco.faireConnaissanceAvec(kumi);
+		marco.listerConnaissances();
+		roro.listerConnaissances();
+		yaku.listerConnaissances();
 	}
 
 }
--- src/personnages/Humain.java
+++ src/personnages/Humain.java
@@ -1,10 +1,13 @@
 package personnages;
 
 public class Humain {
+	public static final int MAX = 30;
 	protected String nom;
 	protected String boissonFavorite;
 	protected int argent;
-	
+	protected int nbConnaissances = 0;
+	protected Humain[] memoire = new Humain[MAX];
+
 	public Humain(String nom, String boissonFavorite, int argent) {
 		super();
 		this.nom = nom;
@@ -23,39 +26,64 @@ public class Humain {
 	public int getArgent() {
 		return argent;
 	}
-	
-	public void DireBonjour() {
-		parler("Bonjour ! Je m'apelle " + nom+ " et j'aime boire du "+ boissonFavorite);
+
+	public void direBonjour() {
+		parler("Bonjour ! Je m'apelle " + nom + " et j'aime boire du " + boissonFavorite);
 	}
-	
-	public void Boire() {parler("Mmmm, un bon verre de "+ boissonFavorite+ "! GLOUPS !");
+
+	public void Boire() {
+		parler("Mmmm, un bon verre de " + boissonFavorite + "! GLOUPS !");
 	}
-	
+
 	public void acheter(String bien, int prix) {
-		if (argent>=prix) {
+		if (argent >= prix) {
 			parler("J'ai " + argent + "sous en poche. Je vais pouvoir m'offrir " + bien + " à " + prix + " sous.");
 			perdreArgent(prix);
-		}
-		else {
-			parler("Je n'ai plus que " + argent + " sous en poche. Je ne peux même pas m'offrir " + bien + " à " + prix + " sous.");
+		} else {
+			parler("Je n'ai plus que " + argent + " sous en poche. Je ne peux même pas m'offrir " + bien + " à " + prix
+					+ " sous.");
 		}
 	}
-	
+
 	public void parler(String texte) {
-		System.out.println( "(" + nom + " ) -  " + texte);
-		}
-	
+		System.out.println("(" + nom + " ) -  " + texte);
+	}
+
 	public void gagnerArgent(int gain) {
 		argent = argent + gain;
 	}
-	
+
 	public void perdreArgent(int perte) {
-		if (argent-perte<0) {
-			argent=0;
+		if (argent - perte < 0) {
+			argent = 0;
+		} else {
+			argent = argent - perte;
 		}
-		else {
-		argent = argent - perte;
+	}
+
+	public void faireConnaissanceAvec(Humain autreHumain) {
+		Humain humain = new Humain(nom, boissonFavorite, argent);
+		direBonjour();
+		autreHumain.repondre(humain);
+		memoriser(autreHumain);
+	}
+
+	private void memoriser(Humain humain) {
+		memoire[nbConnaissances] = humain;
+		nbConnaissances = (nbConnaissances + 1) % MAX;
+	}
+
+	private void repondre(Humain humain) {
+		direBonjour();
+		memoriser(humain);
+	}
+
+	public void listerConnaissances() {
+		String texte = "Je connais beaucoup de monde dont : " + memoire[0].getNom();
+		for (int i=1;i<nbConnaissances;i++) {
+			texte+= ", " + memoire[i].getNom();
 		}
+		System.out.println(texte);
 	}
 	
 }
